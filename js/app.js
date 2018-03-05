8b82c4116aef1d9506c8a7703882ec0715dee123
AmandaMoen <amandalmoen@yahoo.com>
2018-03-04 19:51:30 -0800

    Discovered I forgot the footer row, added, now it works properly.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 'use strict';
 // We need an array to hold the store hours.
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-var all = [];
+
 // We need a table 
 var cookieTable = document.getElementById('cookie-table');
+var cookieForm = document.getElementById('add-location');
 
 // We need a constructor to hold the information for the Cookie Stands.
 // We just need one function of the following: customersEachHour (calcCustomersEachHour), cookiesSoldEachHour 
@@ -16,7 +17,7 @@ function CookieStand(locationName, minCustomersPerHour, maxCustomersPerHour, avg
   this.customersEachHour = [];
   this.cookiesSoldEachHour = [];
   this.totalCookiesSoldPerDay = 0;
-  all.push(this);
+  CookieStand.all.push(this);
 }
 
 // Figure out how many cookies are sold each hour
@@ -51,13 +52,14 @@ CookieStand.prototype.calcCookiesSoldEachHour = function() {
 console.log('my stores ====> ', CookieStand);*/
 
 // Invoke the functions. This will be one of the final steps.
+CookieStand.all = [];
 new CookieStand('Pike Place Market', 23, 65, 6.3, 'pikePlace');
 new CookieStand('SeaTac Airport', 3, 24, 1.2, 'seatac');
 new CookieStand('Seattle Center', 11, 38, 3.7, 'seattleCenter');
 new CookieStand('Capitol Hill', 20, 38, 2.3, 'capitolhill');
 new CookieStand('Alki', 2, 16, 4.6, 'alki');
-// We need to create a table.
 
+// We need to create a table.
 CookieStand.prototype.render = function() {
   this.calcCookiesSoldEachHour();
   var total = 0;
@@ -78,6 +80,23 @@ CookieStand.prototype.render = function() {
   cookieTable.appendChild(row);
 };
 
+function handleForm(e){
+  e.preventDefault();
+  console.log('event object', e);
+  var store = e.target.storeName.value;
+  var min = parseInt(e.target.min.value);
+  var max = parseInt(e.target.max.value);
+  var avg = parseFloat(e.target.avg.value);
+
+  var newStore = new CookieStand(store, min, max, avg);
+
+  e.target.storeName.value = null;
+  e.target.min.value = null;
+  e.target.max.value = null;
+  e.target.avg.value = null;
+
+  createTable();
+}
 // We want to make the header row
 function makeHeaderRow() {
   var trEl = document.createElement('tr');
@@ -92,16 +111,43 @@ function makeHeaderRow() {
   thEl = document.createElement('th');
   thEl.textContent = 'Total';
   trEl.appendChild(thEl);
+  cookieTable.appendChild(trEl);
+}
+
+function makeFooterRow() {
+  var row = document.createElement('tr');
+  var header = document.createElement('th');
+  header.textContent = 'Hourly Totals for All Locations';
+  row.appendChild(header);
+  var totalOfTotals = 0;
+  var hourlyTotal = 0;
+  for (var i = 0; i < hours.length; i++) {
+    var hourlyTotal = 0;
+    for (var j = 0; j < CookieStand.all.length; j++){
+      hourlyTotal += CookieStand.all[j].cookiesSoldEachHour[i];
+      totalOfTotals += CookieStand.all[j].cookiesSoldEachHour[i];
+    }
+    header = document.createElement('th');
+    header.textContent = hourlyTotal;
+    row.appendChild(header);
+  }
+  header = document.createElement('th');
+  header.textContent = totalOfTotals;
+  row.appendChild(header);
+  cookieTable.appendChild(row);
 }
 
 function createTable() {
   makeHeaderRow();
-  for (var i = 0; i < all.length; i++) {
-    all[i].render();
+  for (var i = 0; i < CookieStand.all.length; i++) {
+    CookieStand.all[i].render();
   }
+  makeFooterRow();
 }
 
 createTable();
+
+cookieForm.addEventListener('submit', handleForm);
 
 // The top row needs to autopopulate with hours
 // Rows 2-7 needs to populate with the data from the CookieStand data
